bf653b5a3128049c20dc505e03dbfb31e8175b7a
imnotdannorton <dnorton@dailybreak.com>
2017-06-09 00:41:53 -0400

    use buffer call

--- scripts/stringify.js
+++ scripts/stringify.js
@@ -15,33 +15,47 @@ var s3Params = {
     ContentType: 'image/jpeg',
     ACL: 'public-read'
   };
-  
+
 exports.drawText = function(link, res, q){
   console.log("drawText: ", q);
   var filename = q.trim().split(" ").join("_") + '.jpg';
   request.get(link, function (error, response, body) {
-    gm(body).fill("#FFF").stroke("#000", 1).resize(400).fontSize('30px').font('Impact.ttf').drawText(20, 120,  q.toUpperCase()).stream(function(err, stdout, stderr){
+    gm(body).fill("#FFF").stroke("#000", 1).resize(400).fontSize('30px').font('Impact.ttf').drawText(20, 120,  q.toUpperCase()).setFormat('jpeg').toBuffer(function(err, buffer){
+        console.log("buffer", buffer);
         s3Params.Key = filename;
-        var buf = new Buffer('');
-        stdout.on('data', function(d) {
-           buf = Buffer.concat([buf, d]);
+        s3Params.Body = buffer;
+        s3.putObject(s3Params, (err, s3data) => {
+        if(err){
+          console.log("s3 rrors: ", err);
+          return res.end();
+        }
+        const returnData = {
+          signedRequest: s3data,
+          url: `https://${S3_BUCKET}.s3.amazonaws.com/${filename}`
+        };
+        res.write(JSON.stringify(returnData));
+        res.end();
         });
-        stdout.on('end', function(data) {
-            console.log("putting buf", buf);
-            s3Params.Body = buf;
-            s3.putObject(s3Params, (err, s3data) => {
-            if(err){
-              console.log("s3 rrors: ", err);
-              return res.end();
-            }
-            const returnData = {
-              signedRequest: s3data,
-              url: `https://${S3_BUCKET}.s3.amazonaws.com/${filename}`
-            };
-            res.write(JSON.stringify(returnData));
-            res.end();
-            });
-        });
+        // var buf = new Buffer('');
+        // stdout.on('data', function(d) {
+        //    buf = Buffer.concat([buf, d]);
+        // });
+        // stdout.on('end', function(data) {
+        //     console.log("putting buf", buf);
+        //     s3Params.Body = buf;
+        //     s3.putObject(s3Params, (err, s3data) => {
+        //     if(err){
+        //       console.log("s3 rrors: ", err);
+        //       return res.end();
+        //     }
+        //     const returnData = {
+        //       signedRequest: s3data,
+        //       url: `https://${S3_BUCKET}.s3.amazonaws.com/${filename}`
+        //     };
+        //     res.write(JSON.stringify(returnData));
+        //     res.end();
+        //     });
+        // });
     });
 
   });
