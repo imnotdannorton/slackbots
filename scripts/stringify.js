bfe3b6be7c0f8b91b1f160a12ee463ec64cff966
imnotdannorton <dnorton@dailybreak.com>
2017-06-12 21:52:13 -0400

    clean up

--- scripts/stringify.js
+++ scripts/stringify.js
@@ -14,31 +14,13 @@ var s3Params = {
     ContentType: 'image/jpeg',
     ACL: 'public-read'
   };
-exports.stream = function(link, res, q){
-
-}
 exports.drawText = function(link, res, q){
   console.log("drawText: ", q);
   var filename = q.trim().split(" ").join("_") + '.jpg';
-  // request.get(link, function (error, response, body) {
     console.log(imgMgk);
     imgMgk(request(link)).fill("#FFF").stroke("#000", 1).resize(400).fontSize('30px').font('Impact.ttf').drawText(20, 120,  q.toUpperCase()).setFormat('jpg').stream(function(err, stdout, stderr){
-        // console.log("body: ", body);
-        // console.log("buffer", buffer, body);
+
         s3Params.Key = filename;
-        // s3Params.Body = buffer;
-        // s3.putObject(s3Params, (err, s3data) => {
-        // if(err){
-        //   console.log("s3 rrors: ", err);
-        //   return res.end();
-        // }
-        // const returnData = {
-        //   signedRequest: s3data,
-        //   url: `https://${S3_BUCKET}.s3.amazonaws.com/${filename}`
-        // };
-        // res.write(JSON.stringify(returnData));
-        // res.end();
-        // });
         var buf = [];
         stdout.on('data', function(d) {
             console.log('stdout data: ', d);
@@ -64,7 +46,5 @@ exports.drawText = function(link, res, q){
             console.log(String(data));
         })
     });
-
-  // });
 
 }
